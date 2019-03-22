a3c5f95c9ed1ae856fd9f1820152685e9ea2c21a
MIG <igor123c2019@gmail.com>
2019-03-22 23:44:56 +0200

    * HW3 task4

--- MIG.UIP.HW3.ArraysLoopsConditionalsMethods/Program.cs
+++ MIG.UIP.HW3.ArraysLoopsConditionalsMethods/Program.cs
@@ -36,21 +36,41 @@ namespace MIG.UIP.HW3.ArraysLoopsConditionalsMethods
             Console.WriteLine(" \r\n \r\n Response from task #3  ");
 
             int startCapital = 1000;
-            int percent = 15;
+            int percent = 5;
             int sFinalCapital = 0;
             int kMonths = 0;
 
+            for (int i = 1; startCapital <= 1100; i++) {
+                startCapital = startCapital / 100 * percent + startCapital;
+                kMonths++;               
+                //Console.WriteLine(startCapital);
+                    }
+            Console.WriteLine("Month count = "+kMonths);
+            Console.WriteLine("Final capital "+startCapital );
+
+            // Task#4  (Дано целое число N (> 0). Сформировать и вывести целочисленный массив размера N, содержащий N первых положительных нечетных чисел: 1, 3, 5, … .)
+            Console.WriteLine(" \r\n \r\n Response from task #4  ");
+
+            int n = 20;
+            int[] array = new int [n];
+            for (int i = 1; i < array.Length;i+=2)
+            {
+                array[i] =i;
+                Console.WriteLine(array[i]);
+            }
+
             
+
+
+
+
 
 
             Console.ReadLine();
 
         }
-    }
-    class percent
-    {
-        public double number = 0;
-        public double countPercent = 0;
+
+   
 
        
 
@@ -61,5 +81,5 @@ namespace MIG.UIP.HW3.ArraysLoopsConditionalsMethods
          
       
 
-    }
+    
 }
