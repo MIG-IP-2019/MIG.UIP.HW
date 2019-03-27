6a43f3ef9d52e2246034083cb9652b3c023da8a3
Mig2019 <182zepp@gmail.com>
2019-03-27 18:45:10 +0200

    HW4_(1 task)

---
+++ MIG.HW_4.OOP.Classes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIG.HW_4.OOP.Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // task 1()
+            Console.WriteLine(" \r\n \r\n Response from task #1  ");
+        }
+    }
+}
