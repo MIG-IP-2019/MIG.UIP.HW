0e1938807302269caa13bc97b58a29097a4fb238
MIG <igor123c2019@gmail.com>
2019-04-02 01:33:14 +0300

    * class perimeter/area

---
+++ Rectangle/Rectangle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Rectangle
+    {
+        private double sideA, sideB;
+       
+
+        public Rectangle (double sideA,double sideB)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+        }
+
+        double PerimeterCalculator()
+        {
+            return (sideA + sideB) * 2;
+        }
+        public double Perimeter
+        {
+            get
+            {
+                return this.PerimeterCalculator();
+            }
+        }
+
+        double SquereRectangle()
+        {
+            return (sideA * sideB);
+        }
+
+        public double Squere
+        {
+            get
+            {
+                return this.Squere;
+            }
+        }
+    }
+   
+    
+}
