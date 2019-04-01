06db4563f0c89202bcd1dd45246e8df47faa06a2
Mig2019 <182zepp@gmail.com>
2019-04-01 19:03:19 +0300

    get set (test)

---
+++ Adress/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adress
+{
+    class Address
+    {
+        private int index;
+        private string country;
+        private string city;
+        private string street;
+        private int house;
+        private int apartment;
+
+        public int Index
+        {
+            get { return index; }
+            set { index = value; }
+        }
+
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        public string Street
+        {
+            get { return street; }
+            set { street = value; }
+        }
+
+        public int House
+        {
+            get { return house; }
+            set { house = value; }
+        }
+
+        public int Apartment
+        {
+            get { return apartment; }
+            set { apartment = value; }
+        }
+
+        public void ShowAddress()
+        {
+            Console.WriteLine(index+"  "+country+"  "+city+"  "+street+"  "+house+"  "+apartment);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address adress = new Address();
+            adress.Country = "Ukraine";
+            adress.Index = 03001;
+            adress.City = "Kuiv";
+            adress.Street = "Lucky";
+            adress.House = 17;
+            adress.Apartment = 18;
+            adress.ShowAddress();
+
+            Console.ReadLine();
+        }
+
+        
+    }
+
+   
+}
--- MIG.HW_4.OOP.Classes/Program.cs
+++ MIG.HW_4.OOP.Classes/Program.cs
@@ -1,19 +1,111 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MIG.HW_4.OOP.Classes
+namespace MIG.UIP.HW4.ConditionsArrays
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {       
 
 
-            // task 1()
-            Console.WriteLine(" \r\n \r\n Response from task #1  ");
+           
+            Notebook notebookFirst = new Notebook("Philips", 4, 2.56f);            
+            Notebook notebookSecond = new Notebook("Asahi", 8, 5.24f);
+            Server serverFirst = new Server("HP", 100, 24);
+            Server serverSecond = new Server("DTS", 250, 128);
+
+            Notebook[] notebooks= new Notebook[2] {notebookFirst, notebookSecond};
+            for (int i =0; i<=notebooks.Length-1;i++)
+            {                
+                
+                notebooks[i].DDR = 20;
+                notebooks[i].printProperty();
+            }
+                
+            
+            
+
+
+
+
+            Console.ReadLine();
         }
+
     }
+
+    internal class Computers
+    {
+      
+
+    }
+
+    internal class Notebook
+    {
+        private string model;
+        private int ddr;
+        private float waight;
+        public Notebook(string Model, int DDR, float Waight)
+        {
+            this.model = Model;
+            this.ddr = DDR;
+            this.waight = Waight;
+            //Console.WriteLine(Model + " Notebook  " + DDR + " " + Waight);
+        }
+        public int DDR
+        {
+            set
+            {
+                ddr = ddr;
+            }
+
+        }
+        public void printProperty()
+        {
+            Console.WriteLine(" Notebook  "+ model +" Value= " + ddr + "Gb RAM "+ " Waight=" + waight+"kg");
+        }
+
+       
+       
+
+    }
+    internal class Server
+    {
+        private string name;
+        private int hdd;
+        private int countProcessors;
+        internal Server (string Name, int HDD, int CountProcessors)
+            {
+            this.name = Name;
+            this.hdd = HDD;
+            this.countProcessors = CountProcessors;
+            //Console.WriteLine("Server "+name+  "  Hard drive="+HDD+"Gb "+"Processors="+countProcessors);
+            }
+        public void printProperty()
+        {
+            Console.WriteLine("Server " + name + "  Hard drive=" + hdd + "Gb " + "Processors=" + countProcessors);
+        }
+
+    }
+
+    internal class Tv
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public void printName()
+        {
+            Console.WriteLine(name);
+        }
+
+    }
+             
+
 }
